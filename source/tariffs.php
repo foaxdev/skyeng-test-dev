88483c96fddf4746621187cedb9b9875141a1e17
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-11-22 12:00:28 +0300

    Adds tariffs information

---
+++ source/tariffs.php
@@ -0,0 +1,19 @@
+<?php
+
+$tariffs = array(
+  "tv" => array(
+    "title" => "TV",
+    "description" => "Watch your favorite series at home on large screen with our TV application.",
+    "price" => 20
+  ),
+  "desktop" => array(
+    "title" => "Desktop",
+    "description" => "Enjoy new episodes on your laptop in browser with our web service, which supports all the platforms.",
+    "price" => 15
+  ),
+  "mobile" => array(
+    "title" => "Mobile",
+    "description" => "Get notifications about new releases in our mobile app.",
+    "price" => 10
+  )
+);
