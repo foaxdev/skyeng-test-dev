522a0c7f87bd42db8a97081c6af5a1bbcdac176e
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-11-22 21:53:57 +0300

    Adds tablet and desktop styles and close modal js

--- src/components/card.js
+++ src/components/card.js
@@ -2,8 +2,8 @@ import {tariffs} from "../tariffs";
 
 export const createCardTemplate = tariff => {
   return (`
-    <li class="tariffs-list__item" data-name="${tariff}">
-      <a class="tariffs-list__link">
+    <li class="tariffs-list__item">
+      <a class="tariffs-list__link" href="#" data-name="${tariff}">
         <h3 class="tariffs-list__header">${tariffs[tariff].title}</h3>
         <p class="tariffs-list__description">${tariffs[tariff].description}</p>
         <p class="price">
@@ -11,5 +11,17 @@ export const createCardTemplate = tariff => {
         </p>
       </a>
     </li>
+  `);
+};
+
+export const createModalCardTemplate = tariff => {
+  return (`
+    <a class="tariffs-list__link" href="#" data-name="${tariff}">
+      <h3 class="tariffs-list__header">${tariffs[tariff].title}</h3>
+      <p class="tariffs-list__description">${tariffs[tariff].description}</p>
+      <p class="price">
+        <sup class="price__currency">&#36;</sup>${tariffs[tariff].price}<span class="price__period"> / month</span>
+      </p>
+    </a>
   `);
 };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import {tariffs} from "./tariffs";
-import {createCardTemplate} from "./components/card";
+import {createCardTemplate, createModalCardTemplate} from "./components/card";
+
+const Key = {
+  ESC: 'Escape'
+};
 
 let currentTariff = "tv";
 
-const render = (container, template, place = `beforeend`) => {
+const overlay = document.querySelector('.overlay');
+const tariffsList = document.querySelector('.tariffs-list');
+const modalWindow = document.querySelector('.modal');
+const modalWindowInfo = modalWindow.querySelector('.modal__info');
+const closeButton = modalWindow.querySelector('.modal__close-button');
+
+const render = (container, template, place = 'beforeend') => {
   container.insertAdjacentHTML(place, template);
 };
 
@@ -11,21 +21,43 @@ const clearElement = container => {
   container.innerHTML = '';
 };
 
-const tariffsList = document.querySelector('.tariffs-list');
+const openModalWindow = (tariffCard) => {
+  currentTariff = tariffCard.querySelector('a').getAttribute('data-name');
+  clearElement(modalWindowInfo);
+  render(modalWindowInfo, createModalCardTemplate(currentTariff));
+  modalWindow.classList.add('modal--show');
+  overlay.classList.add('overlay--show');
+  closeButton.addEventListener('click', closeModalWindowByClickHandler);
+  overlay.addEventListener('click', closeModalWindowByClickHandler);
+  document.addEventListener('keydown', closeModalWindowByKeydownHandler);
+};
+
+const closeModalWindowByClickHandler = () => {
+  closeButton.removeEventListener('click', closeModalWindowByClickHandler);
+  overlay.removeEventListener('click', closeModalWindowByClickHandler);
+  modalWindow.classList.remove('modal--show');
+  overlay.classList.remove('overlay--show');
+};
+
+const closeModalWindowByKeydownHandler = (evt) => {
+  if (evt.key === Key.ESC) {
+    closeButton.removeEventListener('click', closeModalWindowByClickHandler);
+    overlay.removeEventListener('click', closeModalWindowByClickHandler);
+    document.removeEventListener('keydown', closeModalWindowByKeydownHandler);
+    modalWindow.classList.remove('modal--show');
+    overlay.classList.remove('overlay--show');
+  }
+};
 
 for (const tariff in tariffs) {
   render(tariffsList, createCardTemplate(tariff));
 }
 
 const tariffCards = document.querySelectorAll('.tariffs-list__item');
-const modalWindow = document.querySelector('.modal');
-const modalWindowInfo = modalWindow.querySelector('.modal__info');
 
 tariffCards.forEach(tariffCard => {
-  tariffCard.addEventListener('click', () => {
-    currentTariff = tariffCard.getAttribute('data-name');
-    clearElement(modalWindowInfo);
-    render(modalWindowInfo, createCardTemplate(currentTariff));
-    modalWindow.classList.add('modal--show');
+  tariffCard.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openModalWindow(tariffCard);
   });
 });
