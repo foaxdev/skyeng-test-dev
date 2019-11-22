4af5e54a10d676719c30c3333f7b040a9b8f9147
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-11-22 11:13:01 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+"use strict";
+
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const csso = require("gulp-csso");
+const terser = require("gulp-terser");
+const server = require("browser-sync").create();
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+
+gulp.task("css", () => {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("js", () => {
+  return gulp.src("source/js/script.js")
+    .pipe(sourcemap.init())
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/js"))
+    .pipe(server.stream());
+});
+
+gulp.task("server", () => {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/js/*.js", gulp.series("js"));
+  gulp.watch(("source/*.php"), gulp.series("copy", "refresh"));
+  gulp.watch(("source/template/**/*.php"), gulp.series("copy", "refresh"));
+});
+
+
+gulp.task("images", () => {
+  return gulp.src("source/img/**/*.{svg}")
+    .pipe(imagemin([
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("copy", () => {
+  return gulp.src([
+    "source/img/**",
+    "source/template/**",
+    "source/*.php",
+    "source/js/*.js",
+    "source/css/*.css"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", () => {
+  return del("build");
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "js"
+));
+
+gulp.task("refresh", done => {
+  server.reload();
+  done();
+});
+
+gulp.task("start", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "js",
+  "server"
+));
